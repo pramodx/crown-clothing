20c39eed158a6e3707ebde8f0021e02cc860807f
Pramod Nair <pramodn@buildxact.com>
2022-09-21 21:31:51 +1000

    Built first category item

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './categories.styles.scss';
+import Directory from './components/directory/directory.component';
 
 const App = () => {
+
   const categories = [
     {
       id: 1,
@@ -30,20 +32,7 @@ const App = () => {
   ];
 
   return (
-    <div className='categories-container'>
-      {categories.map(({ id, title, imageUrl }) => (
-        <div key={id} className='category-container'>
-          <div
-            className='background-image'
-            style={{ backgroundImage: `url(${imageUrl})` }}
-          />
-          <div className='category-body-container'>
-            <h2>{title}</h2>
-            <p>Show Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
+    <Directory categories={categories} />
   );
 };
 
