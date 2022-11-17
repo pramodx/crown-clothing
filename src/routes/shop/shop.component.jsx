aed658125b0e85a089083f6e8fe8a0c35c6bdb5a
Pramod Nair <pramodn@buildxact.com>
2022-11-18 00:28:45 +1100

    category and routing init

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,24 +1,14 @@
-import { Fragment, useContext } from 'react';
-import ProductCard from '../../components/product-card/product-card.component';
-import { CategoriesContext } from './../../context';
+import { Route, Routes } from 'react-router-dom';
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
 import './shop.styles.scss';
 
 const Shop = () => {
-    const { categoriesMap } = useContext(CategoriesContext);
-    console.log('c', categoriesMap);
     return (
-        <Fragment>
-            {Object.keys(categoriesMap).map((title) => (
-                <Fragment key={title}>
-                    <h2>{title}</h2>
-                    <div className='products-container'>
-                        {categoriesMap[title].map((product) => (
-                            <ProductCard key={product.id} product={product} />
-                        ))}
-                    </div>
-                </Fragment>
-            ))}
-        </Fragment>
+        <Routes>
+            <Route index element={<CategoriesPreview />} />
+            <Route path=':category' element={<Category />} />
+        </Routes>
     );
 };
 export default Shop;
